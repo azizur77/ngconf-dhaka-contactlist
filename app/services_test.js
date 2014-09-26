41b2931deaa22225fa1d3621f42e6ae8d761c786
Rifat Nabi <to.rifat@gmail.com>
2014-09-27 03:17:26 +0600

    added SpyOn example

--- app/services_test.js
+++ app/services_test.js
@@ -2,26 +2,58 @@
 
 describe('Contact Service', function() {
 
-  var ContactService;
+  var ContactService, $rootScope;
 
   beforeEach(module('contact.services'));
 
-  beforeEach(inject(function (_ContactService_) {
+  beforeEach(inject(function (_ContactService_, _$rootScope_) {
     ContactService = _ContactService_;
+    $rootScope = _$rootScope_;
+
+    spyOn($rootScope, '$broadcast');
   }));
 
+  describe('Select Contact', function () {
 
-  describe('Toggle Favorite', function() {
-    var contact = {};
+    it('should broadcast contact:selected', function () {
+      ContactService.selectContact({});
+      expect($rootScope.$broadcast).toHaveBeenCalledWith('contact.selected', {});
+    });
 
-    it('should toggle favorite from true to false', function () {
+  });
+
+  describe('Toggle Favorite', function () {
+    var contact;
+
+    beforeEach(function () {
+      contact = {};
+    });
+
+    it('should change true to false', function () {
       contact.favorite = true;
       ContactService.toggleFavorite(contact);
       expect(contact.favorite).toBeFalsy();
     });
 
-    it('should toggle favorite from true to false', function () {
+    it('should change false to true', function () {
       contact.favorite = false;
+      ContactService.toggleFavorite(contact);
+      expect(contact.favorite).toBeTruthy();
+    });
+
+    it('should change null to true', function () {
+      contact.favorite = null;
+      ContactService.toggleFavorite(contact);
+      expect(contact.favorite).toBeTruthy();
+    });
+
+    it('should change 0 to true', function () {
+      contact.favorite = 0;
+      ContactService.toggleFavorite(contact);
+      expect(contact.favorite).toBeTruthy();
+    });
+
+    it('should change undefined to true', function () {
       ContactService.toggleFavorite(contact);
       expect(contact.favorite).toBeTruthy();
     });
